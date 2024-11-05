dce28726408eb7aac2aafbe954642766678ab91c
Habtamu Gashaw <thabtamu417@gmail.com>
2024-11-05 15:28:19 -0600

    finished up the Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Hero from './components/Hero'
-import Workout from './components/Workout'
-import Generator from './components/Generator'
-import { Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import './App.css';
+import Hero from './components/Hero';
+import Workout from './components/Workout';
+import Generator from './components/Generator';
+import AppointmentBooking from './components/Appointment'; // Import the AppointmentBooking component
+import Services from './components/Services'
+
 
 function App() {
-
-  return (<main className='min-h-screen flex flex-col bg-gradient-to-r from-slate-800 to-slate-950 text-white text-sm sm:text-base'>
+  return (
+    <main className="min-h-screen flex flex-col bg-gradient-to-r from-slate-800 to-slate-950 text-white text-sm sm:text-base">
       <Generator />
-      <Hero />
-      <Workout />
-    
+       <Hero />
+       <Services />
+       <AppointmentBooking />
+       <Workout />
+      
+
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
