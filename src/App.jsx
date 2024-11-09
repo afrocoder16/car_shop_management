c71161beea1303acbc78e5ba96e4019689a59da8
Habtamu Gashaw <thabtamu417@gmail.com>
2024-11-09 17:33:39 -0600

    addded the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,15 @@
 import React from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
-import './App.css';
-import Hero from './components/Hero';
-import Workout from './components/Workout';
-import Generator from './components/Generator';
-import AppointmentBooking from './components/Appointment'; // Import the AppointmentBooking component
-import Services from './components/Services'
+import Homepage from './pages/Homepage';
+import Loginpage from './pages/Loginpage';
+import Trackerpage from './pages/Trackerpage';
+import Payment from './pages/Paymentpage';
+import AppointmentBooking from './pages/Appointemntpage';
 
 
 function App() {
   return (
-    <main className="min-h-screen flex flex-col bg-gradient-to-r from-slate-800 to-slate-950 text-white text-sm sm:text-base">
-      <Generator />
-       <Hero />
-       <Services />
-       <AppointmentBooking />
-       <Workout />
-      
-
-    </main>
-  );
+    <Homepage />
+);
 }
 
 export default App;
