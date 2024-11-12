2c517050394520a777bb271df8be38652e0bd562
Habtamu Gashaw <thabtamu417@gmail.com>
2024-11-12 13:14:36 -0600

    done with the frontend of the customer

---
+++ src/pages/ServiceHistorypage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Generator from '../components/Generator';
+import Footer from '../components/Footer';
+import Serivce from '../components/Serivce History';
+
+
+function Servicehistorypage() {
+  return (
+    <main >
+      <Generator />
+       <Serivce />
+       <Footer />
+    </main>
+  );
+}
+
+export default Servicehistorypage;
