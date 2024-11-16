e0ea49b33e15c27ce1d65b8042a2c25211a85e74
Habtamu Gashaw <thabtamu417@gmail.com>
2024-11-16 14:36:01 -0600

    connected the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
-import React from 'react';
-import Homepage from './pages/Homepage';
-import Loginpage from './pages/Loginpage';
-import Trackerpage from './pages/Trackerpage';
-import Payment from './pages/Paymentpage';
-import AppointmentBooking from './pages/Appointemntpage';
-import History from './pages/ServiceHistorypage';
-import ServiceHistory from './components/Serivce History';
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Loginpage from "./pages/Loginpage";
+import Appointmentpage from "./pages/Appointemntpage";
+import Paymentpage from "./pages/Paymentpage";
+import ServiceHistorypage from "./pages/ServiceHistorypage";
+import Trackerpage from "./pages/Trackerpage";
 
 function App() {
   return (
-    <AppointmentBooking />
-);
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/login" element={<Loginpage />} />
+        <Route path="/appointments" element={<Appointmentpage />} />
+        <Route path="/payment" element={<Paymentpage />} />
+        <Route path="/service-history" element={<ServiceHistorypage />} />
+        <Route path="/tracker" element={<Trackerpage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
